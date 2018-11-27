6040bbf359949acb504e5a9b5662300c9bc3ca4c
Bob Fanger <bfanger@gmail.com>
2018-11-27 11:32:47 +0100

    Updated deps

--- test/angular.spec.js
+++ test/angular.spec.js
@@ -4,28 +4,30 @@ const noprocotol = require("../");
 const fixtureDir = __dirname + "/fixtures/";
 
 describe("noprocotol.angular", function() {
-  it("generates a js bundle and a sourcemap file", function(done) {
-    gulp
-      .src(fixtureDir + "controller.js")
-      .pipe(
-        noprocotol.angular({
-          deps: ["ngAnimate"] // Genereate an angular.module()
-        })
-      )
-      .pipe(streamAssert.length(2))
-      .pipe(
-        streamAssert.first(function(file) {
-          expect(file.relative).toBe("app.min.js.map");
-        })
-      )
-      .pipe(
-        streamAssert.second(function(file) {
-          expect(file.relative).toBe("app.min.js");
+  it("generates a js bundle and a sourcemap file", function() {
+    return new Promise(resolve => {
+      gulp
+        .src(fixtureDir + "controller.js")
+        .pipe(
+          noprocotol.angular({
+            deps: ["ngAnimate"] // Genereate an angular.module()
+          })
+        )
+        .pipe(streamAssert.length(2))
+        .pipe(
+          streamAssert.first(function(file) {
+            expect(file.relative).toBe("app.min.js.map");
+          })
+        )
+        .pipe(
+          streamAssert.second(function(file) {
+            expect(file.relative).toBe("app.min.js");
 
-          expect(file.contents.toString()).toMatchSnapshot();
-        })
-      )
-      .on("end", done);
+            expect(file.contents.toString()).toMatchSnapshot();
+          })
+        )
+        .on("end", resolve);
+    });
   });
 
   it("places html files into the $templateCache", function(done) {
